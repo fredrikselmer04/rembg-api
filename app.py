03d991a4e30b8ad9143278573ba03f165af8e93b
fredrikselmer04 <136161285+fredrikselmer04@users.noreply.github.com>
2025-05-22 22:47:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse, JSONResponse
 from rembg import remove
-import requests
 from io import BytesIO
 
 app = FastAPI()
@@ -11,22 +10,10 @@ def index():
     return {"message": "Rembg API is running!"}
 
 @app.post("/remove-bg")
-async def remove_bg(request: Request):
+async def remove_bg(file: UploadFile = File(...)):
     try:
-        data = await request.json()
-        image_url = data.get("image_url")
-
-        if not image_url:
-            return JSONResponse(content={"error": "Missing image_url"}, status_code=400)
-
-        response = requests.get(image_url)
-        if response.status_code != 200:
-            return JSONResponse(content={"error": "Image not downloadable"}, status_code=422)
-
-        input_image = response.content
+        input_image = await file.read()
         output_image = remove(input_image)
-
         return StreamingResponse(BytesIO(output_image), media_type="image/png")
-
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
